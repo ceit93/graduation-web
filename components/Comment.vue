ba1bbdb0e97d3f74afd850d4dc9cbf955bec1af1
Aref <haji4ref@gmail.com>
2018-04-13 00:21:15 +0430

    @ Adds Post and Comments components.

---
+++ components/Comment.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-md-flex mt-2 mb-1">
+    <div class="ml-4">
+      <v-tooltip bottom>
+
+        <span slot="activator">
+          <nuxt-link to="">
+            <v-avatar size="35px" class="elevation-2">
+                <img src="@/static/avatar2.png" alt="">
+            </v-avatar>
+          </nuxt-link>
+        </span>
+
+        <span>احسان موسوی</span>
+      </v-tooltip>
+
+    </div>
+    <div class="speech-bubble bg-main pa-3" style="flex-grow: 1;">
+      آره یادش بخیر خیلی فیلم مزخرفی رفتیم دیدیم. بعدشم بستنی شاد!
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "comment"
+  }
+</script>
+
+<style scoped>
+
+  .speech-bubble {
+    position: relative !important;
+    /*background: #00aabb;*/
+    border-radius: .4em;
+  }
+
+  .speech-bubble:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 40%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-left-color: rgb(237, 241, 245);
+    border-right: 0;
+    border-top: 0;
+    margin-top: -10px;
+    margin-right: -20px;
+  }
+</style>
---
+++ components/Post.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="mb-3">
+    <v-card>
+      <v-card-text>
+        <div class="d-md-flex justify-content-between">
+          <!--user name-->
+          <div class="align-items-end d-md-flex ">
+            <nuxt-link to="">
+              <v-avatar size="40px" class="elevation-2">
+                <img src="@/static/avatar.png" alt="">
+              </v-avatar>
+            </nuxt-link>
+            <span class="mr-2 body-2">{{data.owner.name}}</span>
+          </div>
+          <!--post date-->
+          <div class="g-text-dates">
+            {{data.date | moment('HH:MM jYYYY/jMM/jD') | makeParsi}}
+          </div>
+        </div>
+        <v-divider class="mt-2 mb-2"/>
+        <div style="text-align:justify;">
+          {{data.text}}
+        </div>
+
+
+      </v-card-text>
+      <v-divider/>
+      <!--Comments-->
+      <v-card-text>
+        <comment/>
+        <comment/>
+      </v-card-text>
+
+      <div class="text-md-center">
+        <v-expansion-panel class="text-md-center">
+          <v-expansion-panel-content expand-icon="" class="">
+            <span class="blue--text" slot="header">
+              <span>مشاهده بیشتر</span>
+              <v-icon class="blue--text">mdi-chevron-down</v-icon>
+            </span>
+            <div>
+              <v-card>
+                <v-card-text>
+                  <comment/>
+                  <comment/>
+                  <comment/>
+                </v-card-text>
+
+              </v-card>
+
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </div>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+  import persianJS from 'persianjs';
+  import Comment from '@/components/Comment.vue'
+
+  export default {
+    props: ['data'],
+    name: "post",
+    filters: {
+      makeParsi: function (value) {
+        if (!value) return '';
+        return persianJs(value.toString()).englishNumber().toString();
+      }
+    },
+    components: {
+      Comment
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

a787188266d55fbab8f584e82ec6793835683535
Aref <haji4ref@gmail.com>
2018-05-24 18:22:21 +0430

    @ Adds Login page.

---
+++ layouts/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <div id="cover">
+      <img src="@/static/cover.png" alt="">
+    </div>
+    <nuxt/>
+
+  </v-app>
+
+</template>
+
+<script>
+  export default {
+
+    name: "login"
+  }
+</script>
+
+<style>
+  /*html, body, #__nuxt, #__layout, .login-box-parent {*/
+    /*height: 100%;*/
+  /*}*/
+</style>
---
+++ pages/Login/index.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <v-container fluid class="d-flex">
+    <v-layout class="align-self-center">
+      <v-flex md4 xs12 offset-md4>
+        <v-card>
+          <v-card-title class="blue darken-1 white--text justify-content-center">
+            <span class="title">ورود به حساب</span>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field v-model="stdNumber"
+                            label="شماره دانشجویی"
+                            light
+              >
+
+              </v-text-field>
+
+              <v-text-field v-model="password"
+                            label="رمز عبور"
+                            class="pt-3"
+                            light>
+              </v-text-field>
+              <div class="d-flex justify-content-center">
+                <v-btn
+                  class="success"
+                >
+                  وارد شوید
+                </v-btn>
+              </div>
+
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+
+
+</template>
+
+<script>
+  export default {
+    layout: 'login',
+    name: "index.vue",
+    data() {
+      return {
+        stdNumber: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

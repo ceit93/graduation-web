e784c4d8501e40f370dad0de87db825b07cb0e77
Amir Haghighati <amirh2008@gmail.com>
2018-05-30 17:40:03 +0430

    Materian Icons added.

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,6 +58,7 @@ module.exports = {
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
   },
   css: [

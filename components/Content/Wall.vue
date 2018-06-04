9a06e51e1d408516bc9dbc0e14849bb2d3417a04
Amir Haghighati <amirh2008@gmail.com>
2018-06-05 00:52:38 +0430

    Wall is now restricted

--- components/Content/Wall.vue
+++ components/Content/Wall.vue
@@ -10,9 +10,11 @@
       <h3 class="title">دیوارِ {{user.name}}</h3>
     </v-card-title>
     <v-card-text>
-      <h2 v-if="user.posts === null || user.posts.length === 0" class="grey--text text-xs-center">ای بابا :(</h2>
-      <h4 v-if="user.posts === null || user.posts.length === 0" class="grey--text text-xs-center">هنوز کسی روی دیوار {{user.name}} دل‌نوشته‌ای ننوشته...</h4>
-      <div v-for="(post,index) in user.posts"
+      <h2 v-if="canHaveWall && user.posts === null || user.posts.length === 0" class="grey--text text-xs-center">ای بابا :(</h2>
+      <h4 v-if="canHaveWall && user.posts === null || user.posts.length === 0" class="grey--text text-xs-center">هنوز کسی روی دیوار {{user.name}} دل‌نوشته‌ای ننوشته...</h4>
+      <h2 v-if="!canHaveWall" class="grey--text text-xs-center">شما ۹۳ای نیستید :(</h2>
+      <h4 v-if="!canHaveWall" class="grey--text text-xs-center">امکان داشتن دیوار دل‌نوشته‌ها رو فقط ۹۳ای‌ها دارن. درعوض، شما می‌تونین رو دیوار ۹۳‌ای‌ها براشون دل‌نوشته بنویسید. برای این‌کار می‌تونید به صفحه دل‌نوشته جدید مراجعه کنید.</h4>
+      <div v-if="canHaveWall" v-for="(post,index) in user.posts"
            :key="index">
         <post
           :postData="post"
@@ -34,6 +36,13 @@
       computed: {
         access() {
           return this.user.username === this.$auth.user.username
+        },
+        canHaveWall() {
+          for (let number of this.$auth.user.std_numbers) {
+            if (number.match('^9331[0-9]{3}$'))
+              return true
+          }
+          return false
         }
       },
       notifications: {
@@ -63,7 +72,7 @@
         removed(index) {
           this.$emit('deleted', index)
         }
-      }
+      },
     }
 </script>
 

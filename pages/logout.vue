d4ec3ad26807aa15d560f2bdfa9c4add9ec1f586
Amir Haghighati <amirh2008@gmail.com>
2018-06-06 22:28:48 +0430

    fix logout

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div class="mx-auto d-flex align-items-center" style="height: 100%">
+    <div>
+      <h1 class="blue--text text--lighten-4 text-xs-center">ممنون از حضورتون!</h1>
+      <p class="blue--text text--lighten-4 text-xs-center">لطفا بازم بیاید و ما رو در برگزاری جشنمون یاری کنید.</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +12,7 @@
       name: "logout",
       fetch ({ store, redirect }) {
         if (store.$auth.loggedIn) {
-          this.$auth.logout().then(e => {
+          store.$auth.logout().then(e => {
 
           }).catch(e => {
 

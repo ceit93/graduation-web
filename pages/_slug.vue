13478e303851ea68ee991d89f68363b465d7819d
Amir Haghighati <amirh2008@gmail.com>
2018-06-14 03:36:38 +0430

    add new pages

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,16 +1,15 @@
 <template>
   <v-content>
     <v-container>
-      <v-layout row wrap align-center>
-        <v-flex xs12 md4>
+      <v-layout row wrap align-start>
+        <v-flex xs12 md3>
           <div class="text-xs-center">
-            <v-avatar size="125px">
+            <v-avatar size="130px">
               <img
-                class="img-circle elevation-7 mb-1"
-                :src="this.$helper.avatar(user)"
-              >
+                class="elevation-7 mb-1"
+                :src="this.$helper.avatar(user)">
             </v-avatar>
-            <div class="headline iranblack">{{this.$persianJS.arabicChar(user.name)}}</div>
+            <div class="headline iranblack light-blue--text text--darken-3">{{this.$persianJS.arabicChar(user.name)}}</div>
             <div class="subheading text-xs-center grey--text pt-1 pb-3">{{this.$persianJS.englishNumber(user.std_numbers)}}</div>
             <v-layout justify-space-between>
               <a href="javascript:;" class="body-2">Home</a>
@@ -22,7 +21,7 @@
         </v-flex>
         <v-flex xs12 md5 offset-md2>
           <div v-for="post in user.posts" :key="post._id">
-            <v-card class="my-3" hover>
+            <v-card class="my-3" flat>
               <v-card-media
                 v-if="post.image || post.image !== ''"
                 class="white--text"
@@ -72,7 +71,14 @@
           }).catch(e => {
             context.error({ statusCode: 404, message: 'کاربر مورد نظر یافت می‌نشود...' })
           })
-      }
+      },
+      // fetch ({ store, redirect }) {
+      //   if (store.$auth.loggedIn) {
+      //     return redirect('/profile')
+      //   }
+      //   else
+      //     return redirect('/login')
+      // },
     }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,62 @@
 <template>
-  <div>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs6 sm4 md3 lg2 v-for="(item, index) in studentsArray" :key="index">
+        <div class="text-xs-center">
+          <nuxt-link :to="'/' + item.username">
+            <v-avatar size="120px">
+              <img
+                class="elevation-7 mb-1"
+                :src="$helper.avatar(item)">
+            </v-avatar>
+            <h3 class="iranblack light-blue--text text--darken-3" style="text-decoration: none !important;">{{$persianJS.arabicChar(item.name)}}</h3>
+            <p class="text-xs-center grey--text pt-1 pb-3" style="text-decoration: none;">{{$persianJS.englishNumber(item.std_numbers)}}</p>
+          </nuxt-link>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     name: "index",
-    fetch ({ store, redirect }) {
-      if (store.$auth.loggedIn) {
-        return redirect('/profile')
+    data(){
+      return {
+        students: [],
       }
-      else
-        return redirect('/login')
     },
+    computed: {
+      studentsArray : function() {
+        let res = []
+        for (let person of this.people){
+          person.name = this.$persianJS.userName(person)
+          person.avatar = this.$helper.avatar(person)
+          res.push(person)
+        }
+        res = this.$helper.sortBy(res, 'std_numbers')
+        return res
+      },
+    },
+
+    async asyncData (context) {
+      let {people} = await context.$axios.get('/users/students')
+        .then((res) => {
+          return { people: res.data }
+        }).catch(e => {
+          context.error({statusCode: 500, message: 'خطای سرور...'})
+        })
+      return {
+        people: people
+      }
+    },
+    // fetch ({ store, redirect }) {
+    //   if (store.$auth.loggedIn) {
+    //     return redirect('/profile')
+    //   }
+    //   else
+    //     return redirect('/login')
+    // },
   }
 </script>
 

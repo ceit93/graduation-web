f18dfa6516eb909a5ae428da5c0d91d367daf8cb
Amir Haghighati <amirh2008@gmail.com>
2018-06-15 13:31:36 +0430

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout row wrap align-center justify-center>
+      <v-flex xs11 class="text-xs-center mt-5 mb-5">
+        <h1 class="iranblack blue--text text--darken-3">جامعه دانش‌آموختگان ورودی سال ۱۳۹۳</h1>
+        <h2 class="iranblack">دانشکده مهندسی کامپیوتر و فناوری اطلاعات</h2>
+        <h3 class="iranblack">دانشگاه صنعتی امیرکبیر (پلی‌تکنیک تهران)</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap align-center justify-center>
       <v-flex xs6 sm4 md3 lg2 v-for="(item, index) in studentsArray" :key="index">
-        <div class="text-xs-center">
+        <div class="text-xs-center no-underline">
           <nuxt-link :to="'/' + item.username">
-            <v-avatar size="120px">
+            <v-avatar size="80%">
               <img
                 class="elevation-7 mb-1"
+                width="100%"
                 :src="$helper.avatar(item)">
             </v-avatar>
-            <h3 class="iranblack light-blue--text text--darken-3" style="text-decoration: none !important;">{{$persianJS.arabicChar(item.name)}}</h3>
-            <p class="text-xs-center grey--text pt-1 pb-3" style="text-decoration: none;">{{$persianJS.englishNumber(item.std_numbers)}}</p>
+            <h3 class="iranblack light-blue--text text--darken-3">{{$persianJS.arabicChar(item.name)}}</h3>
+            <p class="text-xs-center grey--text pt-1 pb-3">{{$persianJS.englishNumber(item.std_numbers)}}</p>
           </nuxt-link>
         </div>
       </v-flex>
@@ -50,13 +58,11 @@
         people: people
       }
     },
-    // fetch ({ store, redirect }) {
-    //   if (store.$auth.loggedIn) {
-    //     return redirect('/profile')
-    //   }
-    //   else
-    //     return redirect('/login')
-    // },
   }
 </script>
 
+<style>
+  .no-underline > a{
+    text-decoration: none;
+  }
+</style>

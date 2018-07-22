b7e9838b6d1a0762a7bfd08c04df46a2683d60df
Amir Haghighati <amirh2008@gmail.com>
2018-07-23 02:27:39 +0430

    update helper

--- plugins/helper/helper.js
+++ plugins/helper/helper.js
@@ -3,8 +3,9 @@ export default {
     Vue.prototype.$helper = {
       avatar: function(object) {
         if (object) {
-          if (object.grad_photo)
+          if (object.grad_photo) {
             return object.grad_photo
+          }
           if (object.avatar) {
             if (object.avatar !== '' || object.avatar !== 'undefined')
               return object.avatar

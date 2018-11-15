ccb43181bc7277e7a4c4772d3f299fcfbaccfc35
Amir Haghighati <haghighati.amir@gmail.com>
2018-11-15 18:55:28 -0500

    add d3 update visualization index page

--- pages/visualization/index.vue
+++ pages/visualization/index.vue
@@ -1,10 +1,19 @@
 <template>
-<div></div>
+<div id="svg-canvas"></div>
 </template>
 
 <script>
+    import * as d3 from 'd3'
     export default {
-        name: "index"
+        name: "index",
+        mounted () {
+          console.log(
+            d3.select('#svg-canvas')
+              .append('svg')
+              .attr('width', 200)
+              .attr('height', 100)
+          )
+        }
     }
 </script>
 
